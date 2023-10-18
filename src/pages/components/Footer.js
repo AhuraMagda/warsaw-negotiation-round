f8889fbc693d3f65d5b0e2512ac97077906b3a78
AhuraMagda <lubaszka.magda@gmail.com>
2023-10-18 12:42:09 +0200

    icons

--- src/pages/components/Footer.js
+++ src/pages/components/Footer.js
@@ -6,7 +6,10 @@ function Footer() {
     return (
         <footer>
             <div class="socials">
-                --FB-- --IG-- --PHONE-- --ICON--
+                <img className="socials__icon" src="./icons/icon-facebook.png" alt="facebook"/>
+                <img className="socials__icon" src="./icons/icon-instagram.png" alt="facebook"/>
+                <img className="socials__icon" src="./icons/icon-linkedin.png" alt="facebook"/>
+                <img className="socials__icon" src="./icons/icon-phone.png" alt="facebook"/>
             </div>
             C coś tam Website by MLubaszka
         </footer>
