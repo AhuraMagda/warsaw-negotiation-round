0a2ed540b5f08a90c1e691405002b58fbd1ded27
AhuraMagda <lubaszka.magda@gmail.com>
2023-10-19 15:09:26 +0200

    footer date

--- src/pages/components/Footer.js
+++ src/pages/components/Footer.js
@@ -1,17 +1,21 @@
 
 import React from "react";
 
+const date = `© ${new Date().getFullYear()} Warsaw Negotiation Round`;
 
 function Footer() {
     return (
-        <footer>
-            <div className="socials">
-                <img className="socials__icon" src="./icons/icon-facebook.png" alt="facebook"/>
-                <img className="socials__icon" src="./icons/icon-instagram.png" alt="facebook"/>
-                <img className="socials__icon" src="./icons/icon-linkedin.png" alt="facebook"/>
-                <img className="socials__icon" src="./icons/icon-phone.png" alt="facebook"/>
+        <footer className="footer">
+            <div className="footer__socials">
+                <img className="footer__socials__icon" src="./icons/icon-facebook.png" alt="facebook"/>
+                <img className="footer__socials__icon" src="./icons/icon-instagram.png" alt="facebook"/>
+                <img className="footer__socials__icon" src="./icons/icon-linkedin.png" alt="facebook"/>
+                <img className="footer__socials__icon" src="./icons/icon-phone.png" alt="facebook"/>
             </div>
-            C coś tam Website by MLubaszka
+            <div className="footer__details">
+                <p>{date}</p>
+                <p>Website by <a href="https://github.com/AhuraMagda" target="_blank">M.Lubaszka</a></p>
+            </div>
         </footer>
     )
 }
