2f6ff191cef81711dda92654b3773caabc4a674c
AhuraMagda <lubaszka.magda@gmail.com>
2023-11-10 14:44:14 +0100

    slider dependency fix

--- src/components/Slider.js
+++ src/components/Slider.js
@@ -41,7 +41,7 @@ export default function Slider({
       }, 2500);
     }
     return () => clearInterval(intervalIdRef.current);
-  }, [isInterval, activeSlide]);
+  }, [isInterval, activeSlide, changeSlide, slideData]);
 
   const deleteInterval = () => {
     clearInterval(intervalIdRef.current);
@@ -81,6 +81,7 @@ export default function Slider({
             }}
             className="slider__slide__arrow-left"
             src={leftArrow}
+            alt="arrow icon"
           />
           <img
             onClick={(event) => {
@@ -90,6 +91,7 @@ export default function Slider({
             }}
             className="slider__slide__arrow-right"
             src={rightArrow}
+            alt="arrow icon"
           />
         </div>
       </div>
