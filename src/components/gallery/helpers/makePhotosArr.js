e4684c6bd014ce8e89e5d5de478f9e1cf321576d
AhuraMagda <lubaszka.magda@gmail.com>
2023-11-10 15:56:34 +0100

    handleEnter gallery

--- src/components/gallery/helpers/makePhotosArr.js
+++ src/components/gallery/helpers/makePhotosArr.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { GatsbyImage } from "gatsby-plugin-image";
+import { handleEnter } from "../../helpers/handleEnter";
 
 export const makePhotosArray = (allPhotosData, showImg ) => {
    return allPhotosData.map(({ id, image, key }) => (
@@ -10,6 +11,7 @@ export const makePhotosArray = (allPhotosData, showImg ) => {
           className="gallery__wrapper__card"
           key={key}
           tabIndex="0"
+          onKeyDown={(event)=> handleEnter(event, showImg)}
         >
           <GatsbyImage image={image} alt="students" />
         </div>
