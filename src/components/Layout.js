645dda756165ccbf7ef26c72d91e32ec2f3e9b07
AhuraMagda <lubaszka.magda@gmail.com>
2023-11-11 09:23:42 +0100

    meta

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,15 +1,23 @@
 import React from "react";
 import NavBar from "./navbar/NavBar";
 import Footer from "./Footer";
+import { Helmet } from "react-helmet";
 import "../scss/main-scss/main.css";
 
 function Layout({ children }) {
   return (
-    <div className="site-wrapper">
-      <NavBar />
-      <div className={`content-wrapper`}>{children}</div>
-      <Footer />
-    </div>
+    <>
+      <Helmet>
+      <meta charSet="utf-8" />
+      <title>Warsaw Negotiation Round</title>
+      <meta name="description" content="WNR is one of the most prestigious international negotiation tournaments." />
+      </Helmet>
+      <div className="site-wrapper">
+        <NavBar />
+        <div className={`content-wrapper`}>{children}</div>
+        <Footer />
+      </div>
+    </>
   );
 }
 
