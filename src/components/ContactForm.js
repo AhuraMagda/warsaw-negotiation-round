d4313b20c639edec0ed01efd00a6e3c014ce9fe9
AhuraMagda <lubaszka.magda@gmail.com>
2023-11-24 19:59:17 +0100

    axios

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -1,49 +1,56 @@
 import React from "react";
 import { Formik, useFormik } from "formik";
 import * as Yup from "yup";
-
-// TODO backend
+import axios from "axios";
 
 export default function ContactForm() {
   const formik = useFormik({
     initialValues: {
-      fullName: "",
+      name: "",
       email: "",
       comment: "",
     },
     onSubmit: (values) => {
-      console.log(values);
+      axios.defaults.headers.post["Content-Type"] = "application/json";
+      axios
+        .post("https://formsubmit.co/ajax/bb20869254405cd6094a0972a4746f36", values)
+        .then((response) => console.log(response))
+        .catch((error) => console.log(error));
     },
+
     validationSchema: Yup.object({
-      fullName: Yup.string()
+      name: Yup.string()
         .max(30, "Must be 30 characters or less")
         .required("Required"),
       email: Yup.string().email("Invalid email address").required("Required"),
-      comment: Yup.string().max(300, "Must be 300 characters or less.").required("Required")
+      comment: Yup.string()
+        .max(300, "Must be 300 characters or less.")
+        .required("Required"),
     }),
   });
 
-  console.log(formik.errors);
   return (
     <form
-      action="/functions/send-email.php"
-      method="POST"
       className="contact__form"
       onSubmit={formik.handleSubmit}
     >
       <div className="contact__form__table">
         <div className="contact__form__table__input-wrapper">
-          <label htmlFor="fullName">Full name</label>
+          <label htmlFor="name">Full name</label>
           <input
             type="text"
-            id="fullName"
-            name="fullName"
+            id="name"
+            name="name"
             onChange={formik.handleChange}
             onBlur={formik.handleBlur}
-            value={formik.values.fullName}
+            value={formik.values.name}
             required
           />
-          {formik.touched.fullName && formik.errors.fullName && <p className="contact__form__table__input-wrapper__error">{formik.errors.fullName}</p>}
+          {formik.touched.name && formik.errors.name && (
+            <p className="contact__form__table__input-wrapper__error">
+              {formik.errors.name}
+            </p>
+          )}
         </div>
         <div className="contact__form__table__input-wrapper">
           <label htmlFor="email">E-mail</label>
@@ -51,13 +58,16 @@ export default function ContactForm() {
             type="email"
             id="email"
             name="email"
-            pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$"
             onBlur={formik.handleBlur}
             onChange={formik.handleChange}
             value={formik.values.email}
             required
           />
-          {formik.touched.email && formik.errors.email && <p className="contact__form__table__input-wrapper__error">{formik.errors.email}</p>}
+          {formik.touched.email && formik.errors.email && (
+            <p className="contact__form__table__input-wrapper__error">
+              {formik.errors.email}
+            </p>
+          )}
         </div>
         <div className="contact__form__table__input-wrapper">
           <label htmlFor="comment">Info</label>
@@ -70,7 +80,11 @@ export default function ContactForm() {
             onChange={formik.handleChange}
             value={formik.values.comment}
           ></textarea>
-          {formik.touched.comment && formik.errors.comment && <p className="contact__form__table__input-wrapper__error">{formik.errors.comment}</p>}
+          {formik.touched.comment && formik.errors.comment && (
+            <p className="contact__form__table__input-wrapper__error">
+              {formik.errors.comment}
+            </p>
+          )}
         </div>
         <div>
           <button type="submit">Submit</button>
